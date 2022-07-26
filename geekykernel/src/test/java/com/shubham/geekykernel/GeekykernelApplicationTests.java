fc88c589ff43bf5dfaba40151ad09d6561fbafd0
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2022-07-26 14:09:07 +0530

    Initiated Backend

---
+++ geekykernel/src/test/java/com/shubham/geekykernel/GeekykernelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.geekykernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekykernelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
